d305e9e25d28a319805934fe6231cd7946f8138a
Alex Song <songhongli@gmail.com>
2018-01-08 17:24:19 -0800

    tried to install boilerplate

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,106 +1,42 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+@langrtl
+    <html lang="{{ app()->getLocale() }}" dir="rtl">
+@else
+    <html lang="{{ app()->getLocale() }}">
+@endlangrtl
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta property="og:description" content="页面描述">
-
-        <title>未来编程学院</title>
-
-        <!-- Meta -->
-        <meta name="description" content="硅谷侠">
+        <title>@yield('title', app_name())</title>
+        <meta name="description" content="@yield('meta_description', 'Laravel 5 Boilerplate')">
         <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
         @yield('meta')
 
-        <!-- Styles -->
-        @yield('before-styles')
+        {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
+        @stack('before-styles')
 
         <!-- Check if the language is set to RTL, so apply the RTL layouts -->
         <!-- Otherwise apply the normal LTR layouts -->
-        @langRTL
-            {{ Html::style(getRtlCss(mix('css/frontend.css'))) }}
-        @else
-            {{ Html::style(mix('css/frontend.css')) }}
-        @endif
+        {{ style(mix('css/frontend.css')) }}
 
-        @yield('after-styles')
-
-        <!-- Scripts -->
-        <script>
-            window.Laravel = <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-            ]); ?>
-        </script>
-        <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
-
-        {{--<script>--}}
-            {{--$(function(){--}}
-
-                {{--document.id='normal';--}}
-                {{--var iframe = $('#frame1')[0];--}}
-                {{--var IDoc = iframe.contentDocument || iframe.contentWindow.document; // w/o context--}}
-
-                {{--function addHtml(){--}}
-                    {{--IDoc.open();--}}
-                    {{--IDoc.write($('#html').val());--}}
-                    {{--IDoc.close();--}}
-                {{--}--}}
-
-                {{--function addJS(){--}}
-                    {{--var str   = $('#js').val();--}}
-                    {{--//from $('xxx') to $('#frame1').contents().find('xxx');--}}
-                    {{--var regex = /(\$\(('|"))([a-zA-Z0-9#>=<\[\]:'"_.-]*)(('|")\))/g;--}}
-                    {{--var str= $('#js').val().replace(regex, "$('#frame1').contents().find('$3$4");--}}
-                    {{--//from document to this.frames[0].document--}}
-                    {{--var reg = new RegExp("document", "g");--}}
-                    {{--var str2= str.replace(reg,'this.frames[0].document');--}}
-                    {{--IDoc.fn = new Function(str2);--}}
-                    {{--IDoc.fn();--}}
-                {{--}--}}
-
-                {{--function addStyle (){--}}
-                    {{--var style = document.createElement('style');--}}
-                    {{--style.type = 'text/css';--}}
-                    {{--style.innerHTML =  $('#css').val();--}}
-                    {{--IDoc.getElementsByTagName('head')[0].appendChild(style);--}}
-                {{--}--}}
-
-                {{--$('#go').click(function(){--}}
-                    {{--addHtml();--}}
-                    {{--addJS();--}}
-                    {{--addStyle();--}}
-                {{--});--}}
-            {{--});--}}
-        {{--</script>--}}
-        <style type="text/css">
-            textarea {
-                width: 40%;
-                height: 400px;
-            }
-            iframe{
-                width: 40%;
-                height: 400px;
-            }
-        </style>
+        @stack('after-styles')
     </head>
-    <body id="app-layout">
-    <div style="display:none;"><img src="http://guiguxia.liunar.com/img/1.jpg" width="300" height="300"></div>        <div id="app">
+    <body>
+        <div id="app">
             @include('includes.partials.logged-in-as')
             @include('frontend.includes.nav')
 
             <div class="container">
-                {{--@include('includes.partials.messages')--}}
+                @include('includes.partials.messages')
                 @yield('content')
             </div><!-- container -->
-        </div><!--#app-->
+        </div><!-- #app -->
 
         <!-- Scripts -->
-        @yield('before-scripts')
-        {!! Html::script(mix('js/frontend.js')) !!}
-        @yield('after-scripts')
+        @stack('before-scripts')
+        {!! script(mix('js/frontend.js')) !!}
+        @stack('after-scripts')
 
         @include('includes.partials.ga')
     </body>
