81f697742e4c41a3d237b110cacad3fd30dc978d
Alex Song <songhongli@gmail.com>
2018-01-17 15:42:46 -0800

    微信授权

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Frontend\OpenTokenController;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,7 +19,7 @@ use App\Http\Controllers\Frontend\OpenTokenController;
  *
  *
  */
-Route::get('/details','Api\UserController@details');
+Route::get('/details', 'Api\UserController@details');
 
 Route::get('gettoken', function (Request $request) {
     //传入用户登录code信息 根据传入用户code 到https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code='+ code +'&grant_type=authorization_code',换取openid
@@ -27,7 +27,138 @@ Route::get('gettoken', function (Request $request) {
     //判断opndid是否存在 如果存在则认为用户授权登录，openid不存在则做入库处理 要打通其他平台注册用户和微信注册用为同一用户
     //每次登录都需要走这个流程 获取微信用户信息--》》laravel api 获取openid--》是否注册--》》【注册 入库openid】--》》存在openid  --》》根据openid 拉去token
     //建立token
-    $user = App\User::find(1);
+    if (empty($request->input('code'))) {
+        return response()->json([
+            'status' => 1,
+            'message' => 'invalid code'
+        ]);
+    }
+    $code = $request->input('code');
+
+    $wechatManager = new WechatManager();
+    try {
+        $appid = env('WX_APP_ID');
+        $secret = env('WX_APP_SECRET');
+//第一步:取得openid
+        $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+        $oauth2 = $wechatManager->getJson($oauth2Url);
+        //log::critical("ouath2".$oauth2);
+//第二步:根据全局access_token和openid查询用户信息
+        $access_token = $oauth2["access_token"];
+        $openid = $oauth2['openid'];
+        log::info("openid".$openid);
+        $get_user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
+        $userinfo = $wechatManager->getJson($get_user_info_url);
+        //log::critical("uinfo".$userinfo);
+        $nickname = $userinfo["nickname"];
+        $unionid = $userinfo["openid"];
+        $openid = $userinfo["openid"];
+        log::info("$nickname".$nickname);
+    }catch (\Exception $e) {
+        Log::error('get user exception::' . $e);
+        Log::info('get user exception::end');
+    }
+/*
+    if (!empty($request->input('code')))
+        $code = $request->input('code');
+    else
+        $code = "";
+//        if ($code == "") {
+//            return redirect("/")->with('info', $message);
+//        }
+    $wechatManager = new WechatManager();
+    try {
+        $appid = env('WX_APP_ID');
+        $secret = env('WX_APP_SECRET');
+//第一步:取得openid
+        $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+        $oauth2 = $wechatManager->getJson($oauth2Url);
+        //log::critical("ouath2".$oauth2);
+//第二步:根据全局access_token和openid查询用户信息
+        $access_token = $oauth2["access_token"];
+        $openid = $oauth2['openid'];
+        $get_user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
+        $userinfo = $wechatManager->getJson($get_user_info_url);
+        //log::critical("uinfo".$userinfo);
+        $nickname = $userinfo["nickname"];
+        $unionid = $userinfo["openid"];
+        $openid = $userinfo["openid"];
+//            $user = User::where('wx_unionid', $unionid)->first();
+        $user = User::where('wx_openid', $openid)->first();
+        $newUser = false;
+        $userhelper = app()->make(UserHelper::class);
+        if ($user === null) {
+            Log::info('create new user id.');
+            $user = new User();
+            $request->session()->flush();
+            $request->session()->put('profile_completed_trigger', 'init');
+            $user->password = \Hash::make('the-password-of-choice');
+            $user->email = $unionid . '@fakeemail.com';
+            $user->refer_code = $userhelper->generate_refer_code();
+            $newUser = true;
+        }
+        $user->nickname = $nickname;
+        $user->wx_openid = $openid;
+        $user->wx_headimgurl = $userinfo["headimgurl"];
+        $user->wx_country = $userinfo["country"];
+        $user->wx_province = $userinfo["province"];
+        $user->wx_city = $userinfo["city"];
+        $user->wx_sex = $userinfo["sex"];
+        Log::info('new user. sub');
+        $wxmphelper = app()->make(WxmpHelper::class);
+
+
+//        if(array_key_exists('test', $_GET)){
+
+
+//            if (array_key_exists('subscribe', $userinfo)) {
+//                try {
+//                    Log::info('new user. sub'.$userinfo["subscribe"]);
+//
+//                    $user->wx_subscribe = $userinfo["subscribe"];
+//                } catch (\Exception $e) {
+//
+//                }
+//            }
+        $user->save();
+
+        $token = $wxmphelper->GetAccessToken();
+        $info = $wxmphelper->GetUserInfo($user->wx_openid, $token);
+        if (array_key_exists('subscribe', $info)) {
+            $userhelper->update_subscribe_status($user->wx_openid, $info["subscribe"]);
+        } else {
+            echo 'can not get the field: subscribe';
+        }
+
+        $ori = $request->session()->get('ori', '/');
+
+        if ($newUser) {
+            $request->session()->put('newuser', 'new');
+            $message = self::insert_refer_record($request, $user, $ref);
+            if ($user->wx_subscribe !== 1) {
+                $ori = '/subscribe_wxmp';
+            }
+            Log::info('new user.' . $ori);
+        }
+        $request->session()->put('nickname', $nickname);
+        Log::info('set user id.' . $user->id);
+        $request->session()->put('uid', $user->id);
+
+    } catch (\Exception $e) {
+        Log::error('get user exception::' . $e);
+        $request->session()->put('nickname', 'wx error');
+        $request->session()->put('uid', 'wx error');
+        Log::info('get user exception::end');
+    }
+    if (strpos($ori, 'redeem_refer') !== false) {
+        if ($user->wx_subscribe !== 1) {
+            $ori = '/subscribe_wxmp';
+        }
+        Log::info('reset');
+    }
+    */
+
+    $user = User::find(3);
     $openid = 94589;
     $token = $user->createToken($user['name'])->accessToken;
     $openC = new OpenTokenController();
@@ -37,8 +168,11 @@ Route::get('gettoken', function (Request $request) {
      *把token值入庫保存，加入當前時間+second 作爲token有效時間
      * 这个地方要修改处理返回token 和openid
      */
-
-    return $token;
+    return response()->json([
+        'token' => $token,
+        'openid' => $openid,
+        'status' => 0
+    ]);
 });
 Route::get('fordata', function (Request $request) {
     $openC = new OpenTokenController();
@@ -49,9 +183,9 @@ Route::get('fordata', function (Request $request) {
     $verifyflay = $openC->verfiytime();
     if ($verifyflay) {
         //访问需要访问的控制器 返回需要得数据
-    }else{
+    } else {
         //返回代码错误的时候表示超时需要重新登录重新獲取token
-        return response()->json(['status'=>401,'msg'=>'time out']);
+        return response()->json(['status' => 401, 'msg' => 'time out']);
     }
 
 });
